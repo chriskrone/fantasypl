5b0230d060f0038972b8c764c10764d61f0f88cc
Domas Šiaudvytis <dsiaudvytis@nexient.com>
2018-10-16 23:42:10 -0400

    adds redux, default reducer

---
+++ src/reducers/defaultReducer.js
@@ -0,0 +1,7 @@
+// This is a dummy reducer. Remove once an actual reducer is implemented.
+export default (state = '', { type }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import defaultReducer from './defaultReducer';
+
+export default combineReducers({
+  defaultReducer, // This is a dummy reducer. Remove once an actual reducer is implemented
+});
