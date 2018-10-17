566be68dd4b510d261ba4582542931ed44a36d78
Domas Šiaudvytis <dsiaudvytis@nexient.com>
2018-10-17 14:16:01 -0400

    rename dummy reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import defaultReducer from './defaultReducer';
 
 export default combineReducers({
-  defaultReducer, // This is a dummy reducer. Remove once an actual reducer is implemented
+  reducerCount: defaultReducer, // This is a dummy reducer. Remove once an actual reducer is implemented
 });
