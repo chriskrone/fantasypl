caa1ea8d007ca588424f073adc2963c5f22e78bf
Domas Šiaudvytis <dsiaudvytis@nexient.com>
2018-10-17 13:56:25 -0400

    replace dummy reducer with something less trivial

--- src/reducers/defaultReducer.js
+++ src/reducers/defaultReducer.js
@@ -1,7 +1,9 @@
 // This is a dummy reducer. Remove once an actual reducer is implemented.
-export default (state = '', { type }) => {
+export default (state = 0, { type }) => {
   switch (type) {
+    case 'RESET_REDUCER_COUNT':
+      return 0;
     default:
-      return state;
+      return state + 1;
   }
 }
